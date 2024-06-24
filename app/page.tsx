fb6fd98a7491f1513757ad52a9d3ee82382c75f8
David Mellons <dmellons@gmail.com>
2024-06-23 19:38:19 -0700

    Link cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { auth } from "@/auth";
-import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
-  
+
   return (
     <div
       className="m-auto mt-10 self-center rounded-lg bg-success-200 hover:bg-success-100"
@@ -13,9 +12,11 @@ export default async function Home() {
         Logged in
       </h1>
       <ul className="flex flex-col gap-1 w-2/3">
+        
         {session?.user?.admin &&
-        <li><Link href="/admin" className="hover:underline">Admin</Link></li>
+          <li><Link href="/admin" className="hover:underline">Admin</Link></li>
         }
+
         <li><Link href="/profile" className="hover:underline">Profile</Link></li>
 
       </ul>
