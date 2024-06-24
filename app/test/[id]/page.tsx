4bf7307ef7e253f35bb7e58ee0b769ac9adfc384
David Mellons <dmellons@gmail.com>
2024-06-23 21:01:13 -0700

    Fixed test

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -3,42 +3,53 @@
 import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react"
 
-export default  function Test({params}: {params: {pokemon: string}}) {
-    const pokemon = params.pokemon;
-    const [number, setNumber] = useState('');
+export default  function Test({params}: {params: {id: string}}) {
+    const stuId = params.id;
+    const [number, setNumber] = useState(stuId);
     const [data, setData] = useState({});
     const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-
-        fetchData(number);
-    }, [number])
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNumber(event.target.value);
-    };
     
-    const fetchData = (number: string) => {
-        setLoading(true);
-        fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
+    const fetchData = ({id}: {id: string}) => {
+        console.log(id)
+        // setLoading(true);
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
             .then(res => res.json())
             .then(fetchedData => {
                 console.log(fetchedData)
-                setData(fetchedData)
                 setLoading(false)
+                setData(fetchedData)
             })
             .catch(err => console.error(err))
     }
 
+
+    // useEffect(() => {
+    //         if(stuId) {
+    //         fetchData({id: stuId})
+    //         // setNumber(stuId)
+    //     }
+    // }, [])
+
+    useEffect(() => {
+
+        fetchData({id: number});
+    }, [number])
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value)
+        setNumber(event.target.value);
+    };
+    
+
     console.log(data.name)
 
     return (
-        <div>
+        <div className="w-full">
             Test
             <br/>
             <label htmlFor="pokemon">Number:</label>
             <Input className="w-20 bg-card border-foreground/25"  id="pokemon" type="text" value={number} onChange={handleInputChange} />
             <div
-            className="p-4 m-4 border-2 min-w-1/4 border-primary rounded-lg w-fit"
+            className="p-4 m-4 bg-card min-w-1/2 border-primary rounded-lg w-fit"
             >
             {loading ? 'Loading...' : 
             <div>
