c7bf706a7f83e029ea7189690f4e1369bb5f415b
David Mellons <dmellons@gmail.com>
2024-06-24 20:57:11 -0700

    updated custom query template page

--- app/query/[label]/page.tsx
+++ app/query/[label]/page.tsx
@@ -1,29 +1,34 @@
 import { runQuery } from "@/lib/aeries"
 import { PrismaClient } from "@prisma/client"
+import { Query } from "@prisma/client"
 
 
 const prisma = new PrismaClient()
 export default async function Page({ params }: { params: { label: string } }) {
     const label = params.label
-    const {id, name, query, description, publicQuery, createdBy } = await prisma.query.findUnique({ where: { label: label } })
+    const result = await prisma.query.findUnique({ where: { label: label } }) //const {id, name, query, description, publicQuery, createdBy }:Query | null = await prisma.query.findUnique({ where: { label: label } })
 
-    console.log({id, label, name, query, description, publicQuery, createdBy })
-    
-    const data = runQuery(query)
-    console.log(data)
-    
-    return (
-        <div>
-            <h1 className="text-3xl Underline font-bold">{name}</h1>
-            <label htmlFor="description">Description:</label>
-            <div id='description'>{description}</div>
-            <p>Public/Private: {publicQuery ? "Public" : "Private"}</p>
-            <p>Created By: <a className="hover:underline text-primary" href={`mailto:${createdBy}`}>{createdBy} </a></p>
-            <label htmlFor="query">Query:</label>
-            <br />
-            <code id="query" className="text-sm border bg-card p-2">{query}</code>
-            <h2 className="text-xl underline font-bold mt-2">Data:</h2>
-            <ul>{(await data).map((row) => <li>{JSON.stringify(row, null, 2)}</li>)}</ul>
-        </div>
-    )
+    if (result) {
+
+
+        const data = runQuery(result?.query)
+        console.log(data)
+
+        return (
+            <div>
+                <h1 className="text-3xl Underline font-bold">{result.name}</h1>
+                <label htmlFor="description">Description:</label>
+                <div id='description'>{result.description}</div>
+                <p>Public/Private: {result.publicQuery ? "Public" : "Private"}</p>
+                <p>Created By: <a className="hover:underline text-primary" href={`mailto:${result.createdBy}`}>{result.createdBy} </a></p>
+                <label htmlFor="query">Query:</label>
+                <br />
+                <code id="query" className="text-sm border bg-card p-2">{result.query}</code>
+                <h2 className="text-xl underline font-bold mt-2">Data:</h2>
+                <ul>{(await data).map((row) => <li key={result.id}>{JSON.stringify(row, null, 2)}</li>)}</ul>
+            </div>
+        )
+    } else {
+        return <div>No results found</div>
+    }
 }
