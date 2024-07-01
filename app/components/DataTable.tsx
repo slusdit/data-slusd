c19e4f6da195be9bc1b5c128f54b2cd183f43309
David Mellons <dmellons@gmail.com>
2024-07-01 15:49:49 -0700

    Working datatable 🎉

---
+++ app/components/DataTable.tsx
@@ -0,0 +1,110 @@
+'use client'
+import { useMemo, useState } from 'react';
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  ColumnDef,
+  getSortedRowModel,
+  SortingState
+} from '@tanstack/react-table'
+import { ArrowUp, ArrowDown, ArrowUpDown } from 'lucide-react'
+
+interface DataTableProps<T extends object> {
+  data: T[];
+}
+
+function DT<T extends object>({ data }: DataTableProps<T>) {
+  const [sorting, setSorting] = useState<SortingState>([])
+
+  const columns = useMemo<ColumnDef<T>[]>(() => {
+    if (data.length === 0) return [];
+    return Object.keys(data[0]).map((key) => ({
+      id: key,
+      header: ({ column }) => {
+        return (
+          <div className="flex items-center justify-center">
+            <button
+              className="flex items-center gap-1"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+              <span>{key}</span>
+              {{
+                asc: <ArrowUp className="h-4 w-4 ml-1" />,
+                desc: <ArrowDown className="h-4 w-4 ml-1" />,
+              }[column.getIsSorted() as string] ?? <ArrowUpDown className="h-4 w-4 ml-1" />}
+            </button>
+          </div>
+        )
+      },
+      accessorKey: key,
+    }));
+  }, [data]);
+
+  const reactTable = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  })
+
+  if (data.length === 0) {
+    return <div>No data available</div>
+  }
+
+  return (
+    <table style={{ border: '1px solid black' }}>
+      <thead>
+        {reactTable.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th 
+                key={header.id} 
+                style={{
+                  borderBottom: 'hsl(var(--border))',
+                  background: 'hsl(var(--title))',
+                  color: 'hsl(var(--mainTitle-foreground))',
+                  fontWeight: 'bold',
+                  padding: '8px',
+                  textAlign: 'center',
+                }}
+              >
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {reactTable.getRowModel().rows.map(row => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td 
+                key={cell.id} 
+                style={{
+                  padding: '10px',
+                  border: 'solid 1px hsl(var(--foreground))',
+                  background: 'hsl(var(--card))',
+                  textAlign: 'center',
+                }}
+              >
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default DT;
--- app/query/[category]/[id]/page.tsx
+++ app/query/[category]/[id]/page.tsx
@@ -8,6 +8,7 @@ import { Plus } from "lucide-react";
 import DynamicTable from "@/app/components/DynamicTable";
 import { format } from "sql-formatter";
 import BackButton from "@/app/components/BackButton";
+import DataTable from "@/app/components/DataTable";
 
 const prisma = new PrismaClient();
 export default async function Page({ params }: { params: { id: string } }) {
@@ -17,10 +18,17 @@ export default async function Page({ params }: { params: { id: string } }) {
   const result = await prisma.query.findUnique({ where: { id: id } }); //const {id, name, query, description, publicQuery, createdBy }:Query | null = await prisma.query.findUnique({ where: { label: label } })
   console.log(result);
   if (result) {
-    let data
-    if (process.env.NODE_ENV === "local")
+    let data: any[]
+    console.log(process.env.NODE_ENV)
+    if (process.env.NODE_ENV === "development") 
     {
-      data = format(result.query, { language: "mysql", indent: "  " })
+      data = [
+        { SC:12, ID: 106454388, GR: 8, FN: 'Person 1', LN: 'LN1' },
+        { SC:12, ID: 95455429, GR: 9, FN: 'Person2', LN: 'LN2' },
+        { SC:1, ID: 954558429, GR: 10, FN: 'Person4', LN: 'LN2' },
+
+      ]
+    
     } else {
       data = await runQuery(result?.query);
 
@@ -60,7 +68,7 @@ export default async function Page({ params }: { params: { id: string } }) {
         </p>
         {/* <DataTable columns={columns} data={data} /> */}
         {session?.user?.queryEdit ? (
-          <></>
+          <><DataTable data={data} /></>
           // <QueryInput initialValue={result?.query} initialResult={data} />
         ) : (
           <>
@@ -73,6 +81,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
             <h2 className="text-xl underline font-bold mt-2">Data:</h2>
             <div className="m-4">
+              <DataTable data={data} />
               <DynamicTable data={data} />
             </div>
           </>
