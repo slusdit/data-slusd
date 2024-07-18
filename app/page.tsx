5d1a91e6bc5cc2315d88e4da315d8c939bb9ffce
David Mellons <dmellons@slusd.us>
2024-07-18 10:49:45 -0700

    Started work on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ import { Plus } from "lucide-react";
 import { QueryWithCategory } from "./components/QueryBar";
 import { Separator } from "@/components/ui/separator";
 import { QuerySheet } from "./components/QueiesSheet";
+import { Card } from "@/components/ui/card";
 
 const prisma = new PrismaClient();
 export default async function Home() {
   const session = await auth();
   const queries: QueryWithCategory[] = await prisma.query.findMany({
     select: {
-
       id: true,
       name: true,
       description: true,
@@ -22,88 +22,69 @@ export default async function Home() {
         select: {
           id: true,
           label: true,
-          value: true
-        }
+          value: true,
+        },
       },
-    }
-  })
-
+    },
+  });
 
   let categories;
   if (session?.user) {
-    categories = await prisma.queryCategory.findMany(
-      {
-        include: {
-          queries: true,
-        },
+    categories = await prisma.queryCategory.findMany({
+      include: {
+        queries: true,
       },
-    );
+    });
   }
   return (
-    <div className="m-auto mt-10 self-center rounded-lg bg-success-200 hover:bg-success-100">
-      <h1 className="text-3xl font-weight-800 mb-5">
-        Welcome {session?.user?.name}
-      </h1>
-      {session?.user?.queryEdit && (
-        <FormDialog
-          triggerMessage="Add Query"
-          icon={<Plus className="py-1" />}
-          title="Add Query"
-        >
-          <AddQueryForm session={session} categories={categories} />
-        </FormDialog>
-      )}
-      {session?.user?.admin && (
-        <>
-          {/* <h2 className="font-bold text-2xl mb-4 underline">Pages</h2> */}
-          <ul className="flex flex-col gap-1 w-2/3">
-            <li>
-              <Link href="/admin" className="hover:underline">
-                Admin
-              </Link>
-            </li>
-          </ul>
-          <Separator className="my-4 w-48"/>
-        </>
-      )}
-
-
-      <QuerySheet categories={categories} queries={queries} database={process.env.DB_DATABASE as string} />
-      <h2 className="font-bold text-2xl mb-4 underline">Queries</h2>
-      <ul className="flex flex-col gap-1 w-2/3">
-        {categories && categories.filter((category) => category).map((category) => {
-          if (category) {
-            return (
-
-              <li key={category.id} className="">
-               
-                 <span className="text-xl  font-bold">{category.label}</span>
-
-                <ul>
-                  {queries
-                    .filter((query) => query.category?.value === category.value)
-                    .map((query) => (
-                      <li
-                        key={query.id}
-                        className="ml-4 hover:text-primary hover:underline"
-                      >
-                        <Link
-                          href={`/query/${category.value}/${query.id}`}
-                          className="hover:underline"
-                        >
-                          {query.name}
-                        </Link>
-                      </li>
-                    ))}
-                </ul>
-
+    <div className="m-auto mt-10 self-center flex rounded-lg bg-success-200 hover:bg-success-100">
+      
+      {/* Sidebar */}
+      <div className="w-48 mr-4">
+        <h2 className="font-bold text-center text-lg">Menu</h2>
+        
+        {session?.user?.admin && (
+          <>
+            {/* <h2 className="font-bold text-2xl mb-4 underline">Pages</h2> */}
+            <ul className="flex flex-col gap-1 w-2/3">
+              <li>
+                <Link href="/admin" className="hover:underline">
+                  Admin
+                </Link>
               </li>
-            )
-          }
-        })}
-      </ul>
+            </ul>
+            <Separator className="my-4 w-full" />
+          </>
+        )}
 
+        <QuerySheet
+          categories={categories}
+          queries={queries}
+          database={process.env.DB_DATABASE as string}
+        />
 
+      </div>
+
+      {/* Main Landing Page */}
+      <Card className="w-full p-2 justify-center flex flex-col ">
+      <h1 className="text-3xl font-weight-800 mb-5 text-center">
+        Welcome {session?.user?.name}
+        </h1>
+        {session?.user?.queryEdit && (
+          <div className="w-1/12">
+
+          <FormDialog
+            triggerMessage="Add Query"
+            icon={<Plus className="py-1" />}
+            title="Add Query"
+            
+            >
+            <AddQueryForm session={session} categories={categories} />
+          </FormDialog>
+            </div>
+        )}
+      </Card>
+      
     </div>
   );
 }
