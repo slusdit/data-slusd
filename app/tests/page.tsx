6fa64f4b4ab9a64ed9ff5d4fa8f077689bb2b59b
dmellons <dmellons@slusd.us>
2024-07-19 21:30:54 -0700

    Have working teacher auth middleware in /tests

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,120 +1,149 @@
 import { auth } from "@/auth";
 import RenewSchools from "../components/RenewSchools";
+import { AeriesSimpleStaff, AeriesSimpleTeacher, getAeriesStaff, getTeacherSchoolCredentials } from "@/lib/aeries";
+import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
-type SchoolAccessPermission = {
-    SchoolCode: number;
-    ReadOnlyAccess: boolean;
-    CommunicationGroup: boolean;
-  };
-  
-  type PersonInfo = {
-    SchoolAccessPermissions: SchoolAccessPermission[];
-    ExtendedProperties: any[];
-    EarlyChildhoodCertificationCode: string;
-    Gender: string;
-    EducationLevelCode: string;
-    EthnicityCode: string;
-    RaceCode1: string;
-    RaceCode2: string;
-    RaceCode3: string;
-    RaceCode4: string;
-    RaceCode5: string;
-    PositionStatusCode: string;
-    TotalYearsOfEduService: number;
-    TotalYearsInThisDistrict: number;
-    PreviousLastName: string;
-    PreviousFirstName: string;
-    PreviousMiddleName: string;
-    NameSuffix: string;
-    Address: string;
-    AddressCity: string;
-    AddressState: string;
-    AddressZipCode: string;
-    AddressZipExt: string;
-    HomePhone: string;
-    EmergencyContactName: string;
-    EmergencyContactPhone: string;
-    ID: number;
-    FirstName: string;
-    LastName: string;
-    MiddleName: string;
-    BirthYear: number;
-    BirthDate: string;
-    FullTimePercentage: number;
-    HireDate: string;
-    LeaveDate: string | null;
-    InactiveStatusCode: string;
-    StateEducatorID: string;
-    UserName: string;
-    EmailAddress: string;
-    PrimaryAeriesSchool: number;
-    NetworkLoginID: string;
-    AlternateEmailAddress: string;
-    HumanResourcesSystemID: string;
-    CellPhone: string;
-    NotificationPreferenceCode: string;
-    Title: string;
-  };
 
-type PersonData = {
-    id: number
-    email: string
-    schoolPermissions: SchoolAccessPermission[]
-    primarySchool: number
-    title: string
-}
-
-function getSchools(data: PersonInfo) {
-    if (data.SchoolAccessPermissions.length === 0) return [data.PrimaryAeriesSchool]
-    let schools = data.SchoolAccessPermissions.map(s => s.SchoolCode)
-    return schools
-}
-export async function getAeriesPermissions({
-    endpoint = "/api/v5/staff"
-}: {
-    endpoint?: string
-}) {
-    const session = await auth()
-    const cert = process.env.AERIES_API_KEY as string
-    console.log(cert)
-    
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_AERIES_URL}${endpoint}`,
-        {
-            method: "GET",
-            cache: "force-cache",
-            headers: {
-                "Content-Type": "application/json",
-                "AERIES-CERT": cert
-            },
-        }
-    );
-    const data:PersonInfo[] = await response.json()
-    console.log(data)
-    
-    // const person = data.filter(p => p.EmailAddress === session?.user?.email)[0]
-    // const person = data.filter(p => p.EmailAddress === "mabadia@slusd.us")[0]
-    const person = data.filter(p => p.EmailAddress === "acorona@slusd.us")[0]
-
-    return {
-        "id": person.ID, 
-        "email": person.EmailAddress, 
-        "schoolPermissions": getSchools(person), 
-        "primarySchool": person.PrimaryAeriesSchool, 
-        "title": person.Title
-    }
-
+export function isMatched(obj1: any, array2: any[], key1: string, key2: string) {
+    const objKey = obj1[key1]
+    // console.log({ obj1, array2, key1, key2, objKey });
+    return array2.some(obj2 => {
+        // console.log(obj1[key1] === obj2[key1] && obj1[key2] === obj2[key2])
+        return obj1[key1] === obj2[key1] && obj1[key2] === obj2[key2]
+    });
 }
 
 export default async function Page() {
 
-    const aeriesPermissions = await getAeriesPermissions({endpoint:"/api/v5/staff"});
+    const session = await auth()
+
+    let aeriesPermissions: AeriesSimpleStaff | AeriesSimpleTeacher | null = await getAeriesStaff({ endpoint: "/api/v5/staff" });
+    let aeriesClasses: AeriesSimpleTeacher[] | undefined
+    if (!aeriesPermissions) {
+        return null
+    }
+
+    if (aeriesPermissions.title.toLowerCase() === 'teacher') {
+        aeriesClasses = await getTeacherSchoolCredentials({ id: aeriesPermissions.id, schools: aeriesPermissions.schoolPermissions, })
+
+    }
     console.log(aeriesPermissions)
+    console.log(aeriesClasses)
+
+    // Check DB for current Teacher info with matching PSL == id
+
+    const currentClasses = await prisma.class.findMany({
+        where: {
+            email: aeriesPermissions.email
+        }
+    })
+
+    let displayResult = 'No Acttions'
+    let newAeriesClasses: AeriesSimpleTeacher[] = []
+    // If teachers have current classes in Aeries and current classes in DB
+    if (aeriesClasses && aeriesClasses.length > 0) {
+        if (currentClasses.length > 0) {
+
+            console.log(aeriesClasses.length)
+
+            // Filter out classes that are not in already in the DB
+            newAeriesClasses = aeriesClasses.filter(
+                aeriesClass => {
+                    const test = !isMatched(aeriesClass, currentClasses, 'sc', 'tn')
+                    console.log({ test })
+                    return test
+                }
+            );
+
+
+            const oldCurrentClasses = currentClasses.filter(
+                currentClass => {
+                    const test = !isMatched(currentClass, aeriesClasses, 'sc', 'tn')
+                    console.log({ test })
+                    return test
+                }
+            );
+
+            if (newAeriesClasses.length > 0) {
+                // Add new classes to DB
+                console.log(newAeriesClasses)
+                newAeriesClasses.map(async (aeriesClass) => {
+                    const newClass = await prisma.class.create({ data: aeriesClass })
+                    const result = await prisma.userClass.create({ 
+                        data: {
+                            classId: newClass.id,
+                            userId: session?.user?.id as string,
+                        } 
+                    })
+                    console.log(result)
+                })
+            
+                displayResult = `Some New Classes Added`
+            }
+
+            if (oldCurrentClasses.length > 0) {
+                // Delete old classes from DB
+                console.log(oldCurrentClasses)
+                await Promise.all(oldCurrentClasses.map(async (oldClass) => {
+                    if (oldClass.activeOverride) return null
+                    const result = await prisma.class.delete({
+                        where: {
+                            id: oldClass.id,
+                            activeOverride: false
+                        }
+
+                    })
+                    console.log(result)
+                })
+                )
+                displayResult = 'Out of date classes exists, Old Classes Deleted'
+            }
+        } else {
+            // Add all classes to DB
+            console.log(aeriesClasses)
+            aeriesClasses.map(async (aeriesClass) => {
+                const newClass = await prisma.class.create({ data: aeriesClass })
+                const result = await prisma.userClass.create({ 
+                    data: {
+                        classId: newClass.id,
+                        userId: session?.user?.id as string,
+                    } 
+                })
+                console.log(result)
+            })
+            
+            displayResult = ` classes added - All Classes Added`
+
+        }
+    } else {
+        if (currentClasses.length > 0) {
+            // if no aeries classes and there are current classes then delete all current classes
+            const results = await prisma.class.deleteMany({
+                where: {
+                    email: aeriesPermissions.email,
+                    activeOverride: false
+                    
+                }
+            })
+            console.log(results)
+            displayResult = `${results.count} - All Classes Deleted`
+        } 
+
+    }
+
     return (
         <div>
             <h1>Tests</h1>
-            <RenewSchools email={aeriesPermissions.email} />
+            {/* <RenewSchools fecthFunction={getAeriesStaff}    aeriesApiKey={process.env.AERIES_API_KEY} /> */}
+            <div className="text-blue font-bold">{displayResult}</div>
+            <div className="font-bold underline mt-8">aeriesPermissions</div>
             <pre>{JSON.stringify(aeriesPermissions, null, 2)}</pre>
+            <div className="font-bold underline mt-8">aeriesClasses</div>
+            <pre>{JSON.stringify(aeriesClasses, null, 2)}</pre>
+            <div className="font-bold underline mt-8">currentClasses</div>
+            <pre>{JSON.stringify(currentClasses, null, 2)}</pre>
+
         </div>
     );
 }
--- lib/aeries.ts
+++ lib/aeries.ts
@@ -4,6 +4,116 @@ import { auth } from "@/auth";
 import sql from "mssql";
 import prisma from "./db";
 
+export type SchooolCredentials = {
+  id: number;
+  email: string;
+  sc: number;
+  tn: number;
+  StaffID2: number | null;
+  StaffID3: number | null;
+}
+export type SchoolTeacher = {
+  ClassCalendarSequenceNumber: number;
+  ClassCalendar: string;
+  UserCode1: string;
+  UserCode2: string;
+  UserCode3: string;
+  UserCode4: string;
+  UserCode5: string;
+  UserCode6: string;
+  UserCode7: string;
+  UserCode8: string;
+  LowGrade: number;
+  HighGrade: number;
+  SchoolCode: number;
+  TeacherNumber: number;
+  DisplayName: string;
+  FirstName: string;
+  LastName: string;
+  Room: string;
+  EmailAddress: string;
+  StaffID1: number;
+  StaffID2: number;
+  StaffID3: number;
+  StateCourseCode: string;
+  InactiveStatusCode: string;
+  HighlyQualifiedStatusCode1: string;
+  HighlyQualifiedStatusCode2: string;
+  HighlyQualifiedStatusCode3: string;
+  NextYearInactiveStatusCode: string;
+};
+
+export type SchoolAccessPermission = {
+  SchoolCode: number;
+  ReadOnlyAccess: boolean;
+  CommunicationGroup: boolean;
+};
+
+export type PersonInfo = {
+  SchoolAccessPermissions: SchoolAccessPermission[];
+  ExtendedProperties: any[];
+  EarlyChildhoodCertificationCode: string;
+  Gender: string;
+  EducationLevelCode: string;
+  EthnicityCode: string;
+  RaceCode1: string;
+  RaceCode2: string;
+  RaceCode3: string;
+  RaceCode4: string;
+  RaceCode5: string;
+  PositionStatusCode: string;
+  TotalYearsOfEduService: number;
+  TotalYearsInThisDistrict: number;
+  PreviousLastName: string;
+  PreviousFirstName: string;
+  PreviousMiddleName: string;
+  NameSuffix: string;
+  Address: string;
+  AddressCity: string;
+  AddressState: string;
+  AddressZipCode: string;
+  AddressZipExt: string;
+  HomePhone: string;
+  EmergencyContactName: string;
+  EmergencyContactPhone: string;
+  ID: number;
+  FirstName: string;
+  LastName: string;
+  MiddleName: string;
+  BirthYear: number;
+  BirthDate: string;
+  FullTimePercentage: number;
+  HireDate: string;
+  LeaveDate: string | null;
+  InactiveStatusCode: string;
+  StateEducatorID: string;
+  UserName: string;
+  EmailAddress: string;
+  PrimaryAeriesSchool: number;
+  NetworkLoginID: string;
+  AlternateEmailAddress: string;
+  HumanResourcesSystemID: string;
+  CellPhone: string;
+  NotificationPreferenceCode: string;
+  Title: string;
+};
+
+export type AeriesSimpleStaff = {
+  id: number
+  email: string
+  schoolPermissions: number[]
+  primarySchool: number
+  title: string
+}
+
+export type AeriesSimpleTeacher = {
+  psl: number;
+  email: string;
+  sc: number;
+  tn: number;
+  StaffID2: number | null;
+  StaffID3: number | null;
+}
 // const prisma = new PrismaClient();
 
 // Define the configuration for your SQL Server
@@ -24,10 +134,11 @@ const config = {
 };
 
 // Create a pool instance
+// @ts-ignore
 const poolPromise = new sql.ConnectionPool(config)
   .connect()
   .then((pool) => {
-   
+
     return pool;
   })
   .catch((err) => {
@@ -66,8 +177,8 @@ export async function getSchoolsFromEmail({
   if (schoolQueryResult.recordset && schoolQueryResult.recordset.length !== 0) {
     schoolCode = schoolQueryResult.recordset.map((school) => school.sc);
   }
-  
-  console.log(schoolCode);
+
+  // console.log(schoolCode);
 
   return schoolCode;
 }
@@ -90,7 +201,7 @@ export async function runQuery(
 ) {
   const session = await auth();
   const email = session?.user?.email;
-  const queryBlockList = ["drop", "update", "insert", "delete","modify","alter","create"];
+  const queryBlockList = ["drop", "update", "insert", "delete", "modify", "alter", "create"];
   const queryLower = query?.toLowerCase();
   if (queryBlockList.some((term) => queryLower?.includes(term))) {
     throw Error("Dangerous query");
@@ -109,27 +220,27 @@ export async function runQuery(
     try {
 
       // TEST: Remove email to test @@sc overrice
-      const schoolCode = await getSchoolsFromEmail({ email:email,  pool });
+      const schoolCode = await getSchoolsFromEmail({ email: email, pool });
 
-      console.log(schoolCode);
+      // console.log(schoolCode);
       // TODO: Prepend @@variables to declarations at the top of the query, rather than search and replace?
       // Handle @SC variable
       if (query.includes("@@sc")) {
         if (schoolCode === "0") {
           let allSchools:
             | {
-                sc: string;
-              }[]
+              sc: string;
+            }[]
             | string = await prisma.schoolInfo.findMany({
-            select: {
-              sc: true,
-            },
-          });
+              select: {
+                sc: true,
+              },
+            });
           allSchools = allSchools.map((school) => `${school.sc}`).join(",");
-         ;
+          ;
 
           query = query.replace("= @@sc", `in (${allSchools})`);
-          console.log("Query", query);
+          // console.log("Query", query);
         } else {
           if (typeof schoolCode === "string") {
             query = query.replace("@@sc", schoolCode);
@@ -137,7 +248,7 @@ export async function runQuery(
           if (Array.isArray(schoolCode)) {
             query = query.replace("= @@sc", `in (${schoolCode.join(",")})`); // TODO: make this work with comma separated school codes
           }
-          console.log("Query", query);
+          // console.log("Query", query);
         }
       }
 
@@ -148,7 +259,7 @@ export async function runQuery(
 
       result = await request.query(query);
 
-      console.log("SQL result", result.recordset);
+      // console.log("SQL result", result.recordset);
       // await closePool();
       return result.recordset;
     } catch (error) {
@@ -159,7 +270,7 @@ export async function runQuery(
     }
   } catch (err) {
     console.error("SQL error", err);
-    console.log(query)
+    // console.log(query)
     throw new Error("SQL Pool error", { cause: err });
   }
 }
@@ -169,9 +280,188 @@ export async function closePool() {
   const pool = await poolPromise;
   try {
     await pool.close();
-    console.log("Connection pool closed");
+    // console.log("Connection pool closed");
   } catch (err) {
     console.error("Error closing pool", err);
     throw err;
   }
+}
+
+/**
+ * Retrieves the list of school codes based on the given PersonInfo data.
+ *
+ * @param {PersonInfo} data - The data containing SchoolAccessPermissions and PrimaryAeriesSchool
+ * @return {number[]} The array of school codes extracted from SchoolAccessPermissions
+ */
+function getSchools(data: PersonInfo) {
+  if (data.SchoolAccessPermissions.length === 0) return [data.PrimaryAeriesSchool]
+  let schools = data.SchoolAccessPermissions.map(s => s.SchoolCode)
+  return schools
+}
+
+/*
+ * Helper Functions
+ */
+
+/**
+ * Checks if any object in the array matches the specified keys from the input object.
+ *
+ * @param {any} obj1 - The object to compare keys from.
+ * @param {any[]} array2 - An array of objects to compare against.
+ * @param {string} key1 - The first key to compare.
+ * @param {string} key2 - The second key to compare.
+ * @return {boolean} Returns true if any object in the array matches the specified keys, false otherwise.
+ */
+export async function isMatched(obj1: any, array2: any[], key1: string, key2: string) {
+  const objKey = obj1[key1]
+  // console.log({ obj1, array2, key1, key2, objKey });
+  return array2.some(obj2 => {
+    // console.log(obj1[key1] === obj2[key1] && obj1[key2] === obj2[key2])
+    return obj1[key1] === obj2[key1] && obj1[key2] === obj2[key2]
+  });
+}
+
+/* 
+ * Aeries API Section
+ */
+
+
+
+/**
+ * Retrieves the staff information from the Aeries API.
+ * 
+ *  !! Feature: Look here for admin impersonation 
+ *
+ * @param {Object} options - Optional parameters for the function.
+ * @param {string} options.endpoint - The endpoint to fetch the staff information from. Defaults to "/api/v5/staff".
+ * @return {Promise<Object>} - A promise that resolves to an object containing the staff information.
+ * The object has the following properties:
+ * - id: The ID of the staff member.
+ * - email: The email address of the staff member.
+ * - schoolPermissions: An array of school codes the staff member has permissions for.
+ * - primarySchool: The primary school code of the staff member.
+ * - title: The title of the staff member.
+ */
+export async function getAeriesStaff({
+  endpoint = "/api/v5/staff"
+}: {
+  endpoint?: string
+}) {
+  const session = await auth()
+  const cert = process.env.AERIES_API_KEY as string
+
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_AERIES_URL}${endpoint}`,
+    {
+      method: "GET",
+      cache: "force-cache",
+      headers: {
+        "Content-Type": "application/json",
+        "AERIES-CERT": cert
+      },
+    }
+  );
+  if (response.status !== 200) {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+  const data: PersonInfo[] = await response.json()
+  // console.log(data)
+
+  let person = data.filter(p => p.EmailAddress === session?.user?.email)[0]
+  // person = data.filter(p => p.EmailAddress === "mabadia@slusd.us")[0]
+  // person = data.filter(p => p.EmailAddress === "acorona@slusd.us")[0]
+  // person = data.filter(p => p.EmailAddress === "xbugarin@slusd.us")[0]
+  // person = data.filter(p => p.EmailAddress === "jfox@slusd.us")[0]
+  return {
+    "id": person.ID,
+    "email": person.EmailAddress,
+    "schoolPermissions": getSchools(person),
+    "primarySchool": person.PrimaryAeriesSchool,
+    "title": person.Title
+  }
+
+
+}
+
+
+/**
+ * Retrieves school credentials for a teacher based on their ID and school list.
+ *
+ * @param {number} id - The ID of the teacher
+ * @param {number[]} schools - The list of school IDs
+ * @return {Promise<any>} A Promise that resolves to the teacher information
+ */
+export async function getTeacherSchoolCredentials({
+  id,
+  schools,
+
+}: {
+  id: number
+  schools: number[]
+}) {
+  let teacherInfo
+  if (schools.length > 0) {
+
+    teacherInfo = await Promise.all(schools.map(async (sc) => {
+      // console.log(sc)
+      const schoolCredentials =  await getAeriesSchoolTeacher({ sc, id })
+      // console.log(schoolCredentials)
+      return schoolCredentials
+      
+    })
+    )
+  }
+
+  teacherInfo = teacherInfo?.filter((teacher) => teacher !== null)
+  return teacherInfo
+}
+/**
+ * Retrieves a school teacher from Aeries API based on school code and teacher ID.
+ *
+ * @param {number} sc - The school code of the teacher.
+ * @param {number} id - The ID of the teacher.
+ * @return {Object} An object containing the teacher information.
+ */
+async function getAeriesSchoolTeacher({
+  sc,
+  id
+}: {
+  sc: number
+  id: number
+}) {
+  const cert = process.env.AERIES_API_KEY as string
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_AERIES_URL}/api/v5/schools/${sc}/teachers`,
+    {
+      method: "GET",
+      cache: "force-cache",
+      headers: {
+        "Content-Type": "application/json",
+        "AERIES-CERT": cert
+      },
+    }
+  );
+  // console.log(response)
+  if (response.status !== 200) {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+  const teachers: SchoolTeacher[] = await response.json()
+  // console.log(teachers)
+  // // console.log('data', data)
+  const person: SchoolTeacher = teachers.filter(tch => tch.StaffID1 === id)[0]
+  // console.log(person)
+  if (!person) {
+    return null
+  }
+  const ret = {
+    "psl": person.StaffID1,
+    "email": person.EmailAddress,
+    "sc": person.SchoolCode,
+    "tn": person.TeacherNumber,
+    "StaffID2": person.StaffID2 ? person.StaffID2 : null,
+    "StaffID3": person.StaffID3 ? person.StaffID3 : null
+  }
+  // console.log(ret)
+  return ret
 }
