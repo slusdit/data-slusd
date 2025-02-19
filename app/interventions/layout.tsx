cee4bb18ccc71cff43da06f0600c03e95a0db868
dmellonsSLUSD <dmellons@slusd.us>
2025-02-19 15:20:25 -0800

    Added user permission auth to interventions page

---
+++ app/interventions/layout.tsx
@@ -0,0 +1,38 @@
+import { auth } from "@/auth";
+import UnauthorizedButton from "../components/UnauthorizedButton";
+
+export default async function GradedistributionLayout({
+    children,
+
+  }: Readonly<{
+      children: React.ReactNode;
+      
+  }>) {
+    const session = await auth()
+    const userRoles = session?.user?.roles
+    // console.log(userRoles)
+
+    if (!userRoles?.some(role => [
+        "INTERVENTIONS",
+        "SUPERADMIN",
+        "PRINCIPAL",
+    ].includes(role))) {
+      return (
+        <div>
+          Unauthorized, please go back
+        </div>
+      );
+    }
+
+  
+    return (
+        <div>
+            {session ? children :
+                  <UnauthorizedButton
+                    home
+                  />
+                }
+        </div>
+    );
+  }
+  
